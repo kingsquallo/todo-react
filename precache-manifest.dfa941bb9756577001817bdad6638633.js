b89a307f6a196c7110b35a9b6e9353c53b3558d4
KingSquallo <doanchitaidn@gmail.com>
2020-02-08 09:03:53 +0700

    Updates

---
+++ precache-manifest.dfa941bb9756577001817bdad6638633.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c9dc9f5a19bee31fbc9f05ce72fd0d86",
+    "url": "/todo-react/index.html"
+  },
+  {
+    "revision": "3ed42941994ac75f9bf9",
+    "url": "/todo-react/static/css/main.f51f4f85.chunk.css"
+  },
+  {
+    "revision": "0e5d4e4490df417603e9",
+    "url": "/todo-react/static/js/2.917efc3b.chunk.js"
+  },
+  {
+    "revision": "1eed39b55c0bb7c26ec629a63f87aa2a",
+    "url": "/todo-react/static/js/2.917efc3b.chunk.js.LICENSE"
+  },
+  {
+    "revision": "3ed42941994ac75f9bf9",
+    "url": "/todo-react/static/js/main.5e0f0519.chunk.js"
+  },
+  {
+    "revision": "5a0a6288b6d387935248",
+    "url": "/todo-react/static/js/runtime-main.ce250498.js"
+  }
+]);
